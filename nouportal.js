db88b8c569bd2a4e04191ce89de04fa59bb0e853
Nasokin <boxision@gmail.com>
2023-07-25 18:23:33 +0900

    clickCLELoginButton works

--- nouportal.js
+++ nouportal.js
@@ -1,25 +1,26 @@
 // Function that fills the username and password fields
 function fillCredentials() {
     // Get the username and password
-    browser.storage.sync.get(['username', 'password']).then((res) => {
+    browser.storage.sync.get(["username", "password"]).then((res) => {
         let username = res.username;
         let password = res.password;
 
         // Get the username and password fields
-        let usernameField = document.getElementById('USER_ID');
-        let passwordField = document.getElementById('USER_PASSWORD');
+        let usernameField = document.getElementById("USER_ID");
+        let passwordField = document.getElementById("USER_PASSWORD");
 
         // Check if the fields exist
         if (usernameField && passwordField) {
             // Fill the username field if it's empty
-            if (usernameField.value === '') {
+            if (usernameField.value === "") {
                 usernameField.value = username;
             }
 
             // Fill the password field if it's empty
-            if (passwordField.value === '') {
+            if (passwordField.value === "") {
                 passwordField.value = password;
             }
+            console.log(usernameField.value + " " + passwordField.value);
         } else {
             // If the fields do not exist, try again in 100ms
             setTimeout(() => {
@@ -33,14 +34,14 @@ function fillCredentials() {
 // Function that clicks the login button of the portal page
 function clickPortalLoginButton() {
     // Get all input elements
-    let inputs = document.getElementsByTagName('input');
+    let inputs = document.getElementsByTagName("input");
 
     // Check if there are any input elements
     if (inputs) {
         // Loop through all input elements
         for (let i = 0; i < inputs.length; i++) {
             // If this is the login button, click it
-            if (inputs[i].value.trim() === '&nbsp;Login&nbsp;') {
+            if (inputs[i].value.trim() === "Login") {
                 inputs[i].click();
                 break;
             }
@@ -54,27 +55,34 @@ function clickPortalLoginButton() {
 // Function that clicks the login button of CLE
 function clickCLELoginButton() {
     // Get the login button
-    let loginButton = document.getElementById('loginsaml');
+    let loginButton = document.getElementById("loginsaml");
 
     // Check if the button exists
     if (loginButton) {
         // Click the button
         loginButton.click();
-        console.log('Clicked CLE login button');
     } else {
         // If the button does not exist, try again in 100ms
         setTimeout(clickCLELoginButton, 100);
     }
 }
 
-// Call the appropriate function when the page loads
-window.addEventListener('load', function() {
+function runFunctions() {
+    console.log("runFunctions called");
     // Check the current page URL
-    if (window.location.href.startsWith('https://ou-idp.auth.osaka-u.ac.jp/idp/sso_redirect?')) {
-        // Fill the username and password fields on the portal page
+    if (window.location.href.includes("ou-idp.auth.osaka-u.ac.jp/idp/")) {
         fillCredentials();
-    } else if (window.location.href.startsWith('https://www.cle.osaka-u.ac.jp/')) {
-        // Click the login button on the CLE page
+    } else if (window.location.href.includes("cle.osaka-u.ac.jp")) {
         clickCLELoginButton();
     }
-});
+}
+
+console.log("Script executed");
+
+// Call the appropriate function when the page loads
+if (document.readyState === "complete" || 
+    (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+    runFunctions();
+} else {
+    document.addEventListener("DOMContentLoaded", runMyFunctions);
+}
--- options.js
+++ options.js
@@ -1,19 +1,38 @@
-document.getElementById('settings-form').addEventListener('submit', function(event) {
-    // Prevent the form from submitting
-    event.preventDefault();
-  
-    // Get the username and password
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-  
-    // Save the username and password
-    browser.storage.sync.set({
-      username: username,
-      password: password
-    }).then(() => {;
-        // Display a success message
-        let message = document.createElement('p');
-        message.textContent = 'Saved successfully!';
-        document.body.appendChild(message);
-    });
-  });  
+console.log("Options loaded");
+
+// Set default values for testing
+document.getElementById("username").value = "testname";
+document.getElementById("password").value = "testpass";
+
+document.getElementById("settings-form").addEventListener("submit", function(event) {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Get the username and password
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  console.log("Username: " + username + ", Password: " + password);  
+
+  // Save the username and password
+  browser.storage.sync.set({
+      "username": username,
+      "password": password
+  })
+  .then(() => {
+    // Display a success message
+    let message = document.createElement("p");
+    message.textContent = "Saved successfully!";
+    document.body.appendChild(message);
+
+    // Get and log the stored values
+    return browser.storage.sync.get(["username", "password"]);
+  })
+  .then((res) => {
+    console.log("Stored username: " + res.username);
+    console.log("Stored password: " + res.password);
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
+});
